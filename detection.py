992475c4929bb14d250d8edebbce186daac7c960
Akshay <97163087+akshayparthasarathy@users.noreply.github.com>
2022-01-17 09:44:07 +0800

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,132 @@
+import tensorflow_hub as hub
+import cv2
+import numpy
+import tensorflow as tf
+import pandas as pd
+
+# Carregar modelos
+detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1")
+labels = pd.read_csv('labels.csv',sep=';',index_col='ID')
+labels = labels['OBJECT (2017 REL.)']
+
+location = {}
+
+width = 512
+height = 512
+
+dist = 1
+"""
+# while(True):
+#     #Capture frame-by-frame
+#     ret, frame = cap.read()
+    
+#     #Resize to respect the input_shape
+#     inp = cv2.resize(frame, (width , height))
+
+#     #Convert img to RGB
+#     rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
+
+#     #Is optional but i recommend (float convertion and convert img to tensor image)
+#     rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
+
+#     #Add dims to rgb_tensor
+#     rgb_tensor = tf.expand_dims(rgb_tensor , 0)
+    
+#     boxes, scores, classes, num_detections = detector(rgb_tensor)
+    
+#     pred_labels = classes.numpy().astype('int')[0]
+    
+#     pred_labels = [labels[i] for i in pred_labels]
+#     pred_boxes = boxes.numpy()[0].astype('int')
+#     pred_scores = scores.numpy()[0]
+   
+#    #loop throughout the detections and place a box around it  
+#     for score, (ymin,xmin,ymax,xmax), label in zip(pred_scores, pred_boxes, pred_labels):
+#         if score < 0.5:
+#             continue
+            
+#         score_txt = f'{100 * round(score,0)}'
+#         img_boxes = cv2.rectangle(frame,(xmin, ymax),(xmax, ymin),(0,0,255),2)      
+#         font = cv2.FONT_HERSHEY_SIMPLEX
+#         cv2.putText(img_boxes,label,(xmin, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
+#         cv2.putText(img_boxes,score_txt,(xmax, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
+#         if label == "carrot" or label == "bottle" or label == "apple":
+#             location[label] = [(xmin, ymax),(xmax, ymin)]
+
+#     #Display the resulting frame
+#     cv2.imshow('Recognition',img_boxes)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# print(location)
+# # When everything done, release the capture
+# cap.release()
+# cv2.destroyAllWindows()
+"""
+
+frame = cv2.imread("opencv_frame_0.png")
+
+inp = cv2.resize(frame, (width , height))
+
+#Convert img to RGB
+rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
+
+#Is optional but i recommend (float convertion and convert img to tensor image)
+rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
+
+#Add dims to rgb_tensor
+rgb_tensor = tf.expand_dims(rgb_tensor , 0)
+
+boxes, scores, classes, num_detections = detector(rgb_tensor)
+
+pred_labels = classes.numpy().astype('int')[0]
+
+pred_labels = [labels[i] for i in pred_labels]
+pred_boxes = boxes.numpy()[0].astype('int')
+pred_scores = scores.numpy()[0]
+
+#loop throughout the detections and place a box around it  
+for score, (ymin,xmin,ymax,xmax), label in zip(pred_scores, pred_boxes, pred_labels):
+    if score < 0.3:
+        continue
+        
+    score_txt = f'{100 * round(score,0)}'
+    img_boxes = cv2.rectangle(frame,(xmin, ymax),(xmax, ymin),(0,0,255),2)      
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img_boxes,label,(xmin, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
+    cv2.putText(img_boxes,score_txt,(xmax, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
+    if label == "carrot" or label == "bottle" or label == "apple":
+        location[label] = [((xmin/width)*1, (ymax/height)*1),((xmax/width)*1, (ymin/height)*1)]
+print(location)
+cv2.imshow("Detections", img_boxes)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
+import rob
+
+for i in range(0, len(location)):
+    if location["bottle"] != None:
+        loc = location["bottle"]
+        xmax = loc[1][0]
+        xmin = loc[0][0]
+        ymax = loc[0][1]
+        ymin = loc[1][1]
+        rob.bottle([(xmax+xmin)/2, (ymin+ymax)/2, 0])
+        location["bottle"] = None
+    if location["carrot"] != None:
+        loc = location["carrot"]
+        xmax = loc[1][0]
+        xmin = loc[0][0]
+        ymax = loc[0][1]
+        ymin = loc[1][1]
+        rob.vegetable([(xmax+xmin)/2, (ymin+ymax)/2, 0])
+        location["carrot"] = None
+    if location["apple"] != None:
+        loc = location["apple"]
+        xmax = loc[1][0]
+        xmin = loc[0][0]
+        ymax = loc[0][1]
+        ymin = loc[1][1]
+        rob.fruit([(xmax+xmin)/2, (ymin+ymax)/2, 0])
+        location["apple"] = None
+"""
---
+++ rob.py
@@ -0,0 +1,83 @@
+import urx 
+import cv2 as cv
+from urx.robotiq_two_finger_gripper import Robotiq_Two_Finger_Gripper
+
+
+rob = urx.Robot("192.168.1.6")
+
+robotiqgrip = Robotiq_Two_Finger_Gripper(rob)
+img = cv.imread("bottle_img36.jpg")
+height, width = img.shape()
+
+n = 1
+
+home = [0, -1.57, -1.57, -1.57, 1.57, 0]
+
+bin1 = [-0.20, 0.1, 0.21]
+bin2 = [-0.20, -0.1, 0.21]
+bin3 = [0.20, 0, 0.21]
+
+def bottle(coordinates, _bin=bin1):
+
+    rob.movej(home, 0.2, 0.2)
+    rob.translate_tool(coordinates, 0.1, 0.1)
+
+    robotiqgrip.close_gripper()
+    print("Bottle Grabbed")
+
+    rob.movej(home, 0.2, 0.2)
+    rob.translate_tool(_bin, 0.1, 0.1)
+
+    robotiqgrip.open_gripper()
+    print("Bottle Released")
+
+    rob.movej(home, 0.2, 0.2)
+    print("Bottle moving operation: Complete")
+
+def vegetable(coordinates, _bin=bin2):
+    rob.movej(home, 0.2, 0.2)
+    rob.translate_tool(coordinates, 0.1, 0.1)
+
+    robotiqgrip.close_gripper()
+    print("Vegetable Grabbed")
+
+    rob.movej(home, 0.2, 0.2)
+    rob.translate_tool(_bin, 0.1, 0.1)
+
+    robotiqgrip.open_gripper()
+    print("Vegetable Released")
+
+    rob.movej(home, 0.2, 0.2)
+    print("Vegetable moving operation status: Complete")
+
+def fruit(coordinates, _bin=bin3):
+
+    rob.movej(home, 0.2, 0.2)
+    rob.translate_tool(coordinates, 0.1, 0.1)
+
+    robotiqgrip.close_gripper()
+    print("Fruit Grabbed")
+
+    rob.movej(home, 0.2, 0.2)
+    rob.translate_tool(_bin, 0.1, 0.1)
+
+    robotiqgrip.open_gripper()
+    print("fruit Released")
+
+    rob.movej(home, 0.2, 0.2)
+    print("Fruit moving operation status: Complete")
+
+test = [0, 0, 0.21]
+
+bottle(test)
+
+vegetable(test)
+
+fruit(test)
+
+for i in range(0, n):
+    input(0, n)
+
+rob.close()
+
+#try using force stop sensor
